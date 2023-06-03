98e86db84eaa1c2d71b334d0016e23a6babb2216
Vicodiru <marve.vicodiru@gmail.com>
2023-06-03 12:41:12 +0100

    icon update

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function setItemToEdit(item) {
     .querySelectorAll("li")
     .forEach((i) => i.classList.remove("edit-mode"));
   item.classList.add("edit-mode");
-  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"><i/> Update Item';
   formBtn.style.backgroundColor = "#228B22";
   itemInput.value = item.textContent;
 }
